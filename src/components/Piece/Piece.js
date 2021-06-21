332c8894f557f837e4e649a38c4f91d2b609031b
ahmedali0159 <pirates0159@gmail.com>
2021-06-22 00:06:46 +0600

    added food item images

---
+++ src/components/Piece/Piece.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Piece.css';
+
+class Piece extends Component {
+    render() {
+        return (
+            <div>
+                <ul className="pieces">
+                    {this.props.foods.map((food) => (
+                        <li key={food._id}>
+                            <div className="piece">
+                                <a style={{textDecoration: 'none'}} href={"#" + food._id}>
+                                    <img src={food.image} alt={food.title}/>
+                                    <p style={{fontSize: '1.2rem', color: 'black', textAlign:'center'}}>
+                                        {food.title}
+                                    </p>
+                                </a>
+                                <div className="piece-price">
+                                    <div>
+                                        {food.price}
+                                    </div>
+                                    <button className="button primary">
+                                        Add To Card
+                                    </button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Piece;
